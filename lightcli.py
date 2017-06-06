c005ac8eac81054571fb49f818af9f38641442ab
Dan Persons <dpersonsdev@gmail.com>
2017-06-05 22:29:03 -0400

    Update string_input()

--- lightcli.py
+++ lightcli.py
@@ -35,7 +35,7 @@ __version__ = '1.0'
 def string_input(prompt):
     """Python 3 input()/Python 2 raw_input()"""
     v = sys.version[0]
-    if v == 3:
+    if v == '3':
         return input(prompt)
     else:
         return raw_input(prompt)
